b6eeb7894e772aa4e89bccfd361701ebcfe44e4b
emre cil <info.emrecil@gmail.com>
2023-05-16 15:15:45 +0300

    remove eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import * as path from 'path';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {},
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'ViteButton',
+      fileName: (format) => `index.${format}.js`,
     },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
   },
+  plugins: [react(), dts()],
 });
